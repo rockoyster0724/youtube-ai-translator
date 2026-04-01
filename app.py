5ad79570d70f07c8e811595ea0910a794e4697a3
rockoyster0724 <rockoyster0724@gmail.com>
2026-04-01 10:10:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ import concurrent.futures
 from groq import Groq
 from deep_translator import GoogleTranslator
 st.set_page_config(
-    page_title="生蠔 YT 翻譯機",
-    page_icon="🦪",
+    page_title="生蠔YT翻譯機",
+    page_icon="Gemini_Generated_Image_dxmz5adxmz5adxmz",
     layout="centered"
 )
 # --- 輔助函數：將秒數轉換為標準時間格式 ---
